9bc14cf8fb69ddba3bd13e847130abff9e538ec1
Evan <emcgrael@outlook.com>
2025-02-19 14:30:19 -0500

    made rendering objects more modular

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <GLFW/glfw3.h>
 #include <vector>
 #include <iostream>
+#include <string>
 
 const char *vertexShaderSource = "#version 330 core\n"
                                  "layout(location = 0) in vec3 aPos;\n"
@@ -14,7 +15,7 @@ const char *vertexShaderSource = "#version 330 core\n"
                                  "uniform mat4 view;\n"
                                  "uniform mat4 projection;\n"
                                  "void main() {\n"
-                                 "    gl_Position = projection * view * model * vec4(aPos, 5.0);\n"
+                                 "    gl_Position = projection * view * model * vec4(aPos, 1.0);\n"
                                  "}";
 
 const char *fragmentShaderSource = "#version 330 core\n"
@@ -22,6 +23,12 @@ const char *fragmentShaderSource = "#version 330 core\n"
                                    "void main() {\n"
                                    "    FragColor = vec4(0.8274, 0.8313, 0.8588, 1.0);\n"
                                    "}";
+
+struct Model
+{
+    unsigned int VAO, VBO, EBO;
+    std::vector<unsigned int> indices;
+};
 
 unsigned int createShader(unsigned int type, const char *source)
 {
@@ -31,20 +38,20 @@ unsigned int createShader(unsigned int type, const char *source)
     return shader;
 }
 
-std::vector<float> vertices;
-std::vector<unsigned int> indices;
-
-bool loadOBJ(const char *path)
+Model loadOBJ(const char *path)
 {
     tinyobj::attrib_t attrib;
     std::vector<tinyobj::shape_t> shapes;
     std::vector<tinyobj::material_t> materials;
     std::string warn, err;
 
+    Model model;
+    std::vector<float> vertices;
+
     if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, path))
     {
         std::cerr << "Failed to load OBJ file: " << warn << err << std::endl;
-        return false;
+        return model;
     }
 
     for (const auto &shape : shapes)
@@ -54,10 +61,39 @@ bool loadOBJ(const char *path)
             vertices.push_back(attrib.vertices[3 * index.vertex_index + 0]);
             vertices.push_back(attrib.vertices[3 * index.vertex_index + 1]);
             vertices.push_back(attrib.vertices[3 * index.vertex_index + 2]);
-            indices.push_back(indices.size());
+            model.indices.push_back(model.indices.size());
         }
     }
-    return true;
+
+    glGenVertexArrays(1, &model.VAO);
+    glGenBuffers(1, &model.VBO);
+    glGenBuffers(1, &model.EBO);
+
+    glBindVertexArray(model.VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, model.VBO);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, model.EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, model.indices.size() * sizeof(unsigned int), model.indices.data(), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
+    return model;
+}
+
+void renderObj(unsigned int modelloc, const char *path, glm::vec3 position, glm::vec3 rotation, glm::vec3 size)
+{
+    Model model = loadOBJ(path);
+    glm::mat4 objModel = glm::translate(glm::mat4(1.0f), position);
+    objModel = glm::rotate(objModel, glm::radians(rotation.x), glm::vec3(1.0, 0.0f, 0.0f));
+    objModel = glm::rotate(objModel, glm::radians(rotation.y), glm::vec3(0.0, 1.0f, 0.0f));
+    objModel = glm::rotate(objModel, glm::radians(rotation.z), glm::vec3(0.0, 0.0f, 1.0f));
+    objModel = glm::scale(objModel, size);
+    glUniformMatrix4fv(modelloc, 1, GL_FALSE, glm::value_ptr(objModel));
+    glBindVertexArray(model.VAO);
+    glDrawElements(GL_TRIANGLES, model.indices.size(), GL_UNSIGNED_INT, 0);
 }
 
 int main()
@@ -67,28 +103,10 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow *window = glfwCreateWindow(800, 600, "OBJ Renderer", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Rendering Test", NULL, NULL);
     glfwMakeContextCurrent(window);
     gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
     glEnable(GL_DEPTH_TEST);
-
-    if (!loadOBJ("assets/cube.obj"))
-        return -1;
-
-    unsigned int VAO, VBO, EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-    glEnableVertexAttribArray(0);
 
     unsigned int vertexShader = createShader(GL_VERTEX_SHADER, vertexShaderSource);
     unsigned int fragmentShader = createShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
@@ -99,21 +117,17 @@ int main()
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    glm::mat4 model = glm::mat4(1.0f);
-    float angle = glm::radians(15.0f);
     glm::mat4 view = glm::lookAt(
-        glm::vec3(3.0f * sin(angle), 0.0f, 3.0f * cos(angle)),
+        glm::vec3(3.0f, 3.0f, 3.0f),
         glm::vec3(0.0f, 0.0f, 0.0f),
-        glm::vec3(0.0f, 1.0f, 0.0f)
-    );
-    glm::mat4 projection = glm::perspective(glm::radians(30.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+        glm::vec3(0.0f, 1.0f, 0.0f));
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
     unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
     unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
     unsigned int projectionLoc = glGetUniformLocation(shaderProgram, "projection");
 
     glUseProgram(shaderProgram);
-    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
     glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
     glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
@@ -121,8 +135,10 @@ int main()
     {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+        
+        renderObj(modelLoc, "assets/duck.obj", glm::vec3(1.5f, 0.0f, 0.0f), glm::vec3(0.0f, 110.0f, 0.0f), glm::vec3(2.0f, 2.0f, 2.0f));
+        renderObj(modelLoc, "assets/cube.obj", glm::vec3(-1.0f, -0.5f, 0.0f), glm::vec3(30.0f, 0.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f));
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
