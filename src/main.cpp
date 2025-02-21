870e234ba5fd6a5e6835aab978692e67843aeb53
Evan <emcgrael@outlook.com>
2025-02-20 21:04:11 -0500

    lighting and shadows gotta clean up project next

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+
 #define TINYOBJLOADER_IMPLEMENTATION
 #include "tiny_obj_loader.h"
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <vector>
@@ -12,34 +14,119 @@
 #include <string>
 #include <cmath>
 
-const char *vertexShaderSource = "#version 330 core\n"
-                                 "layout(location = 0) in vec3 aPos;\n"
-                                 "layout(location = 1) in vec2 aTexCoord;\n"
-                                 "uniform mat4 model;\n"
-                                 "uniform mat4 view;\n"
-                                 "uniform mat4 projection;\n"
-                                 "out vec2 TexCoord;\n"
-                                 "void main() {\n"
-                                 "    gl_Position = projection * view * model * vec4(aPos, 1.0);\n"
-                                 "    TexCoord = aTexCoord;\n"
-                                 "}\n";
+const char *vertexShaderSource = R"(
+#version 330 core
+layout(location = 0) in vec3 aPos;
+layout(location = 1) in vec3 aNormal;
+layout(location = 2) in vec2 aTexCoord;
 
-const char *fragmentShaderSource = "#version 330 core\n"
-                                   "out vec4 FragColor;\n"
-                                   "in vec2 TexCoord;\n"
-                                   "uniform sampler2D texture1;\n"
-                                   "void main() {\n"
-                                   "    FragColor = texture(texture1, TexCoord);\n"
-                                   "}\n";
+out vec3 FragPos;
+out vec3 Normal;
+out vec2 TexCoord;
+out vec4 FragPosLightSpace;
 
-struct Model
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+uniform mat4 lightSpaceMatrix;
+
+void main()
 {
+    vec4 worldPos = model * vec4(aPos, 1.0);
+    FragPos = worldPos.xyz;
+    Normal = mat3(transpose(inverse(model))) * aNormal;
+    TexCoord = aTexCoord;
+    FragPosLightSpace = lightSpaceMatrix * worldPos;
+    
+    gl_Position = projection * view * worldPos;
+}
+)";
+
+const char *fragmentShaderSource = R"(
+#version 330 core
+out vec4 FragColor;
+
+in vec3 FragPos;
+in vec3 Normal;
+in vec2 TexCoord;
+in vec4 FragPosLightSpace;
+
+uniform sampler2D texture1;
+uniform sampler2D shadowMap;
+
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+uniform vec3 lightColor;
+uniform vec3 objectColor;
+
+float ShadowCalculation(vec4 fragPosLightSpace)
+{
+    vec3 projCoords = fragPosLightSpace.xyz / fragPosLightSpace.w;
+    projCoords = projCoords * 0.5 + 0.5;
+    
+    float closestDepth = texture(shadowMap, projCoords.xy).r;
+    float currentDepth = projCoords.z;
+    
+    float bias = max(0.05 * (1.0 - dot(normalize(Normal), normalize(lightPos - FragPos))), 0.005);
+    
+    float shadow = currentDepth - bias > closestDepth ? 1.0 : 0.0;
+    
+    if(projCoords.z > 1.0)
+        shadow = 0.0;
+        
+    return shadow;
+}
+
+void main()
+{
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * lightColor;
+    
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+    
+    float specularStrength = 0.5;
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specularStrength * spec * lightColor;
+    
+    float shadow = ShadowCalculation(FragPosLightSpace);
+    
+    vec3 lighting = ambient + (1.0 - shadow) * (diffuse + specular);
+    vec3 texColor = texture(texture1, TexCoord).rgb;
+    vec3 result = lighting * texColor;
+    FragColor = vec4(result, 1.0);
+}
+)";
+
+const char *depthVertexShaderSource = R"(
+#version 330 core
+layout(location = 0) in vec3 aPos;
+uniform mat4 model;
+uniform mat4 lightSpaceMatrix;
+void main()
+{
+    gl_Position = lightSpaceMatrix * model * vec4(aPos, 1.0);
+}
+)";
+
+const char *depthFragmentShaderSource = R"(
+#version 330 core
+void main()
+{
+    
+}
+)";
+
+struct Model {
     unsigned int VAO, VBO, EBO;
     std::vector<unsigned int> indices;
 };
 
-struct Renderable
-{
+struct Renderable {
     Model model;
     unsigned int texture;
 };
@@ -55,10 +142,30 @@ unsigned int createShader(unsigned int type, const char *source)
     {
         char infoLog[512];
         glGetShaderInfoLog(shader, 512, NULL, infoLog);
-        std::cerr << "ERROR::SHADER::COMPILATION_FAILED\n"
-                  << infoLog << std::endl;
+        std::cerr << "ERROR::SHADER::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
     return shader;
+}
+
+unsigned int createShaderProgram(const char *vertexSource, const char *fragmentSource)
+{
+    unsigned int vertexShader = createShader(GL_VERTEX_SHADER, vertexSource);
+    unsigned int fragmentShader = createShader(GL_FRAGMENT_SHADER, fragmentSource);
+    unsigned int program = glCreateProgram();
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+    glLinkProgram(program);
+    int success;
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if(!success)
+    {
+        char infoLog[512];
+        glGetProgramInfoLog(program, 512, NULL, infoLog);
+        std::cerr << "ERROR::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    return program;
 }
 
 unsigned int loadTexture(const char *path)
@@ -121,6 +228,20 @@ Model loadOBJ(const char *path)
             vertices.push_back(attrib.vertices[3 * index.vertex_index + 0]);
             vertices.push_back(attrib.vertices[3 * index.vertex_index + 1]);
             vertices.push_back(attrib.vertices[3 * index.vertex_index + 2]);
+
+            if (index.normal_index >= 0)
+            {
+                vertices.push_back(attrib.normals[3 * index.normal_index + 0]);
+                vertices.push_back(attrib.normals[3 * index.normal_index + 1]);
+                vertices.push_back(attrib.normals[3 * index.normal_index + 2]);
+            }
+            else
+            {
+                vertices.push_back(0.0f);
+                vertices.push_back(0.0f);
+                vertices.push_back(1.0f);
+            }
+
             if (index.texcoord_index >= 0)
             {
                 vertices.push_back(attrib.texcoords[2 * index.texcoord_index + 0]);
@@ -131,6 +252,7 @@ Model loadOBJ(const char *path)
                 vertices.push_back(0.0f);
                 vertices.push_back(0.0f);
             }
+
             model.indices.push_back(static_cast<unsigned int>(model.indices.size()));
         }
     }
@@ -140,17 +262,17 @@ Model loadOBJ(const char *path)
     glGenBuffers(1, &model.EBO);
 
     glBindVertexArray(model.VAO);
-
     glBindBuffer(GL_ARRAY_BUFFER, model.VBO);
     glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
-
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, model.EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, model.indices.size() * sizeof(unsigned int), model.indices.data(), GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 
     glBindVertexArray(0);
 
@@ -165,18 +287,32 @@ Renderable loadRenderable(const char *objPath, const char *texturePath)
     return renderable;
 }
 
-void renderObj(unsigned int modelLoc, const Renderable &renderable,
+void renderObj(unsigned int shaderProgram, const Renderable &renderable,
                glm::vec3 position, glm::vec3 rotation, glm::vec3 size)
 {
-    glm::mat4 objModel = glm::translate(glm::mat4(1.0f), position);
-    objModel = glm::rotate(objModel, glm::radians(rotation.x), glm::vec3(1.0f, 0.0f, 0.0f));
-    objModel = glm::rotate(objModel, glm::radians(rotation.y), glm::vec3(0.0f, 1.0f, 0.0f));
-    objModel = glm::rotate(objModel, glm::radians(rotation.z), glm::vec3(0.0f, 0.0f, 1.0f));
-    objModel = glm::scale(objModel, size);
-    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(objModel));
+    glm::mat4 model = glm::translate(glm::mat4(1.0f), position);
+    model = glm::rotate(model, glm::radians(rotation.x), glm::vec3(1.0f, 0.0f, 0.0f));
+    model = glm::rotate(model, glm::radians(rotation.y), glm::vec3(0.0f, 1.0f, 0.0f));
+    model = glm::rotate(model, glm::radians(rotation.z), glm::vec3(0.0f, 0.0f, 1.0f));
+    model = glm::scale(model, size);
+    glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
 
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, renderable.texture);
+
+    glBindVertexArray(renderable.model.VAO);
+    glDrawElements(GL_TRIANGLES, renderable.model.indices.size(), GL_UNSIGNED_INT, 0);
+}
+
+void renderObjDepth(unsigned int shaderProgram, const Renderable &renderable,
+                    glm::vec3 position, glm::vec3 rotation, glm::vec3 size)
+{
+    glm::mat4 model = glm::translate(glm::mat4(1.0f), position);
+    model = glm::rotate(model, glm::radians(rotation.x), glm::vec3(1.0f, 0.0f, 0.0f));
+    model = glm::rotate(model, glm::radians(rotation.y), glm::vec3(0.0f, 1.0f, 0.0f));
+    model = glm::rotate(model, glm::radians(rotation.z), glm::vec3(0.0f, 0.0f, 1.0f));
+    model = glm::scale(model, size);
+    glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
 
     glBindVertexArray(renderable.model.VAO);
     glDrawElements(GL_TRIANGLES, renderable.model.indices.size(), GL_UNSIGNED_INT, 0);
@@ -188,8 +324,8 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    GLFWwindow *window = glfwCreateWindow(800, 600, "Rendering Test", NULL, NULL);
+    
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Rendering Test", NULL, NULL);
     if (!window)
     {
         std::cerr << "Failed to create GLFW window" << std::endl;
@@ -197,61 +333,130 @@ int main()
         return -1;
     }
     glfwMakeContextCurrent(window);
+    
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cerr << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+    
     glEnable(GL_DEPTH_TEST);
-
-    unsigned int vertexShader = createShader(GL_VERTEX_SHADER, vertexShaderSource);
-    unsigned int fragmentShader = createShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
-    unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-    glUseProgram(shaderProgram);
-    glUniform1i(glGetUniformLocation(shaderProgram, "texture1"), 0);
-
-    glm::mat4 view = glm::lookAt(glm::vec3(3.0f, 3.0f, 3.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-    glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
-
-    unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
-    unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
-    unsigned int projectionLoc = glGetUniformLocation(shaderProgram, "projection");
-
-    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-    glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
-
     stbi_set_flip_vertically_on_load(true);
 
-    glfwSwapInterval(0);
-
+    unsigned int finalShaderProgram = createShaderProgram(vertexShaderSource, fragmentShaderSource);
+    unsigned int depthShaderProgram = createShaderProgram(depthVertexShaderSource, depthFragmentShaderSource);
+    
+    glUseProgram(finalShaderProgram);
+    glUniform1i(glGetUniformLocation(finalShaderProgram, "texture1"), 0);
+    
+    const unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;
+    unsigned int depthMapFBO;
+    glGenFramebuffers(1, &depthMapFBO);
+    
+    unsigned int depthMap;
+    glGenTextures(1, &depthMap);
+    glBindTexture(GL_TEXTURE_2D, depthMap);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT,
+                 SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+    float borderColor[] = {1.0f, 1.0f, 1.0f, 1.0f};
+    glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+    
+    glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
+    glDrawBuffer(GL_NONE);
+    glReadBuffer(GL_NONE);
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    
+    glm::vec3 cameraPos(3.0f, 3.0f, 3.0f);
+    glm::mat4 view = glm::lookAt(cameraPos,
+                                 glm::vec3(0.0f, 0.0f, 0.0f),
+                                 glm::vec3(0.0f, 1.0f, 0.0f));
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f/600.0f, 0.1f, 100.0f);
+    
+    glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+    float near_plane = 1.0f, far_plane = 20.0f;
+    glm::mat4 lightProjection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, near_plane, far_plane);
+    glm::mat4 lightView = glm::lookAt(lightPos,
+                                      glm::vec3(0.0f, 0.0f, 0.0f),
+                                      glm::vec3(0.0f, 1.0f, 0.0f));
+    glm::mat4 lightSpaceMatrix = lightProjection * lightView;
+    
+    glUseProgram(finalShaderProgram);
+    glUniformMatrix4fv(glGetUniformLocation(finalShaderProgram, "lightSpaceMatrix"), 1, GL_FALSE, glm::value_ptr(lightSpaceMatrix));
+    
+    glUniform3f(glGetUniformLocation(finalShaderProgram, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
+    glUniform3f(glGetUniformLocation(finalShaderProgram, "viewPos"), cameraPos.x, cameraPos.y, cameraPos.z);
+    glUniform3f(glGetUniformLocation(finalShaderProgram, "lightColor"), 1.0f, 1.0f, 1.0f);
+    glUniform3f(glGetUniformLocation(finalShaderProgram, "objectColor"), 1.0f, 0.5f, 0.31f);
+    
+    glUseProgram(finalShaderProgram);
+    glUniform1i(glGetUniformLocation(finalShaderProgram, "shadowMap"), 1);
+    
     Renderable cubeRenderable = loadRenderable("assets/cube.obj", "assets/concrete.png");
     Renderable brickRenderable = loadRenderable("assets/cube.obj", "assets/brick.png");
     Renderable duckRenderable = loadRenderable("assets/duck.obj", "assets/duck.jpg");
-
+    
+    glfwSwapInterval(0);
     double lastTime = glfwGetTime();
     int frameCount = 0;
-
+    
     while (!glfwWindowShouldClose(window))
     {
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glUseProgram(shaderProgram);
-
-        renderObj(modelLoc, cubeRenderable, glm::vec3(0.0f, -2.0f, 0.0f), glm::vec3(90.0f, 0.0f, 0.0f), glm::vec3(20.0f, 20.0f, 0.1f));
-
-        renderObj(modelLoc, brickRenderable, glm::vec3(-4.0f, -1.0f, -10.0f), glm::vec3(90.0f, 0.0f, 0.0f), glm::vec3(1.0f, 14.0f, 5.0f));
-
+        glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+        glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+        glClear(GL_DEPTH_BUFFER_BIT);
+        glUseProgram(depthShaderProgram);
+        glUniformMatrix4fv(glGetUniformLocation(depthShaderProgram, "lightSpaceMatrix"), 1, GL_FALSE, glm::value_ptr(lightSpaceMatrix));
+        
+        renderObjDepth(depthShaderProgram, cubeRenderable,
+                       glm::vec3(0.0f, -2.0f, 0.0f),
+                       glm::vec3(90.0f, 0.0f, 0.0f),
+                       glm::vec3(20.0f, 20.0f, 0.1f));
+        renderObjDepth(depthShaderProgram, brickRenderable,
+                       glm::vec3(-4.0f, -1.0f, -10.0f),
+                       glm::vec3(90.0f, 0.0f, 0.0f),
+                       glm::vec3(1.0f, 14.0f, 5.0f));
         float duckX = sin(glfwGetTime() * 0.5f) * 5.0f;
-        renderObj(modelLoc, duckRenderable, glm::vec3(duckX, -2.0f, 0.0f), glm::vec3(0.0f, 80.0f, 0.0f), glm::vec3(2.0f, 2.0f, 2.0f));
-
+        renderObjDepth(depthShaderProgram, duckRenderable,
+                       glm::vec3(duckX, -2.0f, 0.0f),
+                       glm::vec3(0.0f, 80.0f, 0.0f),
+                       glm::vec3(2.0f, 2.0f, 2.0f));
+        
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        
+        glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+        glViewport(0, 0, 800, 600);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram(finalShaderProgram);
+        glUniformMatrix4fv(glGetUniformLocation(finalShaderProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(glGetUniformLocation(finalShaderProgram, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+        glUniformMatrix4fv(glGetUniformLocation(finalShaderProgram, "lightSpaceMatrix"), 1, GL_FALSE, glm::value_ptr(lightSpaceMatrix));
+        glUniform3f(glGetUniformLocation(finalShaderProgram, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
+        glUniform3f(glGetUniformLocation(finalShaderProgram, "viewPos"), cameraPos.x, cameraPos.y, cameraPos.z);
+        
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, depthMap);
+        
+        renderObj(finalShaderProgram, cubeRenderable,
+                  glm::vec3(0.0f, -2.0f, 0.0f),
+                  glm::vec3(90.0f, 0.0f, 0.0f),
+                  glm::vec3(20.0f, 20.0f, 0.1f));
+        renderObj(finalShaderProgram, brickRenderable,
+                  glm::vec3(-4.0f, -1.0f, -10.0f),
+                  glm::vec3(90.0f, 0.0f, 0.0f),
+                  glm::vec3(1.0f, 14.0f, 5.0f));
+        renderObj(finalShaderProgram, duckRenderable,
+                  glm::vec3(duckX, -2.0f, 0.0f),
+                  glm::vec3(0.0f, 80.0f, 0.0f),
+                  glm::vec3(2.0f, 2.0f, 2.0f));
+        
         glfwSwapBuffers(window);
         glfwPollEvents();
-
+        
         double currentTime = glfwGetTime();
         frameCount++;
         if (currentTime - lastTime >= 1.0)
@@ -261,7 +466,7 @@ int main()
             lastTime = currentTime;
         }
     }
-
+    
     glfwTerminate();
     return 0;
 }
